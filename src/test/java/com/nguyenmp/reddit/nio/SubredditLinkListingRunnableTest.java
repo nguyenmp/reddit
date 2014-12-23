db7093b7c4c258910cab8ae4d33e6c3394455e10
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-12-22 16:22:42 -0800

    Expanded testing of Subreddit listing fetching

--- src/test/java/com/nguyenmp/reddit/nio/SubredditLinkListingRunnableTest.java
+++ src/test/java/com/nguyenmp/reddit/nio/SubredditLinkListingRunnableTest.java
@@ -1,5 +1,8 @@
 package com.nguyenmp.reddit.nio;
 
+import com.nguyenmp.reddit.data.Link;
+import com.nguyenmp.reddit.data.LinkData;
+import com.nguyenmp.reddit.data.SubredditLinkListing;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,23 +12,39 @@ public class SubredditLinkListingRunnableTest {
     public void testGetEndpointNulls() throws Exception {
         SubredditLinkListingRunnable subredditFetcher = new SubredditLinkListingRunnable.Builder().build();
         Assert.assertEquals(subredditFetcher.getEndpoint(), ".json");
+        SubredditLinkListing result = subredditFetcher.call();
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void testGetEndpointSubreddit() throws Exception {
         SubredditLinkListingRunnable subredditFetcher = new SubredditLinkListingRunnable.Builder().forSubreddit("wtf").build();
         Assert.assertEquals(subredditFetcher.getEndpoint(), "r/wtf/.json");
+        SubredditLinkListing result = subredditFetcher.call();
+        Assert.assertNotNull(result);
+        Link[] children = result.getData().getChildren();
+        Assert.assertTrue(children.length > 0);
+        for (Link link : children) {
+            LinkData data = link.getData();
+            Assert.assertEquals("WTF", data.getSubreddit());
+        }
     }
 
     @Test
     public void testGetEndpointAfter() throws Exception {
         SubredditLinkListingRunnable subredditFetcher = new SubredditLinkListingRunnable.Builder().after("t3_12355").build();
         Assert.assertEquals(subredditFetcher.getEndpoint(), ".json?after=t3_12355");
+        SubredditLinkListing result = subredditFetcher.call();
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void testGetEndpointAfterSubreddit() throws Exception {
-        SubredditLinkListingRunnable subredditFetcher = new SubredditLinkListingRunnable.Builder().forSubreddit("aweee").after("t3_12955").build();
-        Assert.assertEquals(subredditFetcher.getEndpoint(), "r/aweee/.json?after=t3_12955");
+        SubredditLinkListingRunnable subredditFetcher = new SubredditLinkListingRunnable.Builder().forSubreddit("aww").after("t3_12955").build();
+        Assert.assertEquals(subredditFetcher.getEndpoint(), "r/aww/.json?after=t3_12955");
+        SubredditLinkListing result = subredditFetcher.call();
+        Assert.assertNotNull(result);
+        Link[] children = result.getData().getChildren();
+        Assert.assertEquals(0, children.length);
     }
 }
