9880e5ce10b57a8790c4fdbed508cfa5237aac2f
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-12-23 12:45:34 -0800

    Added my reddit username to the user agent field

--- src/main/java/com/nguyenmp/reddit/Config.java
+++ src/main/java/com/nguyenmp/reddit/Config.java
@@ -4,5 +4,5 @@ public class Config {
     public static final String USERNAME = "_reader";
     public static final String PASSWORD = USERNAME;
 
-    public static final String USER_AGENT = "com.nguyenmp.reddit/0.0.0-testing";
+    public static final String USER_AGENT = "com.nguyenmp.reddit/0.0.0-testing by /u/markerz";
 }
